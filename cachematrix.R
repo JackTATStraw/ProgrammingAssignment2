8fe494bb6af51c143cbaef40329f2c2442ab9564
JackTATStraw <ahfyyyh@outlook.com>
2015-12-26 13:35:43 +0800

    Assignment 2 for R Programming
    Finished version

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
+## inv == inversed matrix
+## return a list that contains three functions, which stands for the special "matrix" object
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    get <- function() x
+    setinv <- function(invmat) inv <<- invmat
+    getinv <- function() inv
+    
+    list(get = get,
+         getinv = getinv,
+         setinv = setinv)
 }
 
 
 ## Write a short comment describing this function
 
+## mat stands for matrix
+## first get the cached result. calculate if it's null, else return the cached result
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message("Getting cached results")
+        return(inv)
+    }
+    
+    mat <- x$get()
+    inv <- solve(mat)
+    x$setinv(inv)
+    inv
 }
